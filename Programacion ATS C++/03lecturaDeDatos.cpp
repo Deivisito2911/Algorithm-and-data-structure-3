ae7140547fec48c0bd6a1cab6aaa5aa6781d42f6
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-03 18:08:54 -0400

    Lectura de datos y comentarios

---
+++ Programacion ATS C++/03lecturaDeDatos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+/*
+Este comentario
+es largo
+ */
+int main(){
+    int numero = 0;//Variable para numero
+    cout<<"Ingrese un numero: "<<endl;
+    cin>> numero;
+
+    cout<<"\nEl numero fue : "<<numero;
+
+    return 0;
+}
