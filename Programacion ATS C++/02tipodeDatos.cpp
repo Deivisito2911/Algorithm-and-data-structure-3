e18008dd2224454b396bb3ea1719b8eaf89ebf6a
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-03 18:02:15 -0400

    Tipo de datos

---
+++ Programacion ATS C++/02tipodeDatos.cpp
@@ -0,0 +1,18 @@
+//Tipos de datos
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num = 15;
+    float decimal = 17.5;
+    double mayor = 17.5646;
+    char letra = 'a';
+    cout<<num<<endl;
+    cout<<decimal<<endl;
+    cout<<mayor<<endl;
+    cout<<letra<<endl;
+
+    return 0;
+}
