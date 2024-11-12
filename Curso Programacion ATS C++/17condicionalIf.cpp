a2cfbbaa91b4703f9d2df78e150a3a75eb1b4c5b
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-12 19:20:12 -0400

    Condicional If

---
+++ Curso Programacion ATS C++/17condicionalIf.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//operadores de condicionales ==, !=, >=, <=, <, >
+int main(){
+    int numero = 0, dato = 5;
+    cout<<"Indique un numero: "<<endl;
+    cin>>numero;
+    if (numero!=dato){
+        cout<<"El numero es diferente de 5";
+    }else{
+        cout<<"El numero es cinco";
+    }
+    return 0;
+}
