b3e2c8dfc4f90a1cfdf3a7f9acc456af64e29b90
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-12 11:02:27 -0400

    Ejercicio Lectura y muestra de datos

---
+++ Programacion ATS C++/06lecturaYMuestraDeDatos.cpp
@@ -0,0 +1,26 @@
+/*Realice un programa que lea de la entrada estandar los siguientes datos de 
+una persona:
+    Edad: dato de tipo entero.
+    Sexo: dato de tipo caracter.
+    Altura en metros: dato de tipo real.
+    
+Tras leer los datos, el programa debe mostrarlos en la salida estandar.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int edad = 0;
+    char sexo[10];
+    float altura = 0;
+
+    cout<<"Indiqueme la edad: "<<endl;
+    cin>>edad;
+    cout<<"Indiqueme su sexo: "<<endl;
+    cin>>sexo;
+    cout<<"Indiqueme su altura: "<<endl;
+    cin>>altura;
+    cout<<"Datos de la persona: "<<endl;
+    cout<<"Edad: "<<edad<<"\nSexo: "<<sexo<<"\nAltura: "<<altura<<endl;
+}
