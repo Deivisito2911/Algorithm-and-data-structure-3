4bf0370c0fc4d246cb0164a981ec9095d3743c74
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-12 11:46:45 -0400

    Expresiones algebraicas

---
+++ Programacion ATS C++/09expresiones3.cpp
@@ -0,0 +1,23 @@
+//3. Escribe la siguiente expresion como expresion en c++ a+b/c / d+e/f
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a, b, c, d, e, f, resultado = 0;
+    cout<<"Introduce el valor de a: "<<endl;
+    cin>>a;
+    cout<<"Introduce el valor de b: "<<endl;
+    cin>>b;
+    cout<<"Introduce el valor de c: "<<endl;
+    cin>>c;
+    cout<<"Introduce el valor de d: "<<endl;
+    cin>>d;
+    cout<<"Introduce el valor de e: "<<endl;
+    cin>>e;
+    cout<<"Introduce el valor de f: "<<endl;
+    cin>>f;
+    resultado = ((a+(b/c))/(d+(e/f)));
+    cout<<"El resultado es: "<<resultado<<endl;
+    return 0;
+}
---
+++ Programacion ATS C++/10expresiones4.cpp
@@ -0,0 +1,19 @@
+//Escriba la siguiente expresion como expresion en c++ a+ b/c-d
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a, b, c, d, resultado = 0;
+    cout<<"Introduzca el valor de a: "<<endl;
+    cin>>a;
+    cout<<"Introduzca el valor de b: "<<endl;
+    cin>>b;
+    cout<<"Introduzca el valor de c: "<<endl;
+    cin>>c;
+    cout<<"Introduzca el valor de d: "<<endl;
+    cin>>d;
+    resultado = (a+(b/(c-d)));
+    cout<<"El resultado es: "<<resultado<<endl;
+    return 0;
+}
