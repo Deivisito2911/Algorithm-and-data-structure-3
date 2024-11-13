2fc4a82c17ee1c7ab54ca23188d03a4cfa0e53c1
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-12 21:16:31 -0400

    Verificar si es vocal mayuscula o minuscula

---
+++ Curso Programacion ATS C++/24vocalMinusculaMayuscula.cpp
@@ -0,0 +1,27 @@
+/*Escriba un pograma que lea de la entrada estandar un caracter e indique 
+en la salida estandar si el caracter es una vocal minuscula, es una vocal
+mayuscula o no es una vocal*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    char letra = ' ';
+    cout<<"Indique una letra: "<<endl;
+    cin>>letra;
+    switch(letra){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u': cout<<"Es una vocal minuscula"<<endl;
+        break;
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U': cout<<"Es una vocal mayuscula"<<endl;
+        break;
+        default: cout<<"No es una vocal"<<endl;
+    }
+    return 0;
+}
