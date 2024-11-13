366f5322a337742e1646b06af05b66ec174b96f5
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-12 22:25:33 -0400

    Cajero automatico

---
+++ Curso Programacion ATS C++/28cajeroAutomatico.cpp
@@ -0,0 +1,46 @@
+/*Hacer un programa que simule un cajero automatico con un saldo inicial
+de 1000 dolares*/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    const int saldo_inicial = 1000;
+    float saldo = saldo_inicial, aux = 0;
+    int opc = 0;
+    do{
+        cout<<"\t Bienvenido a Banco Daniela"<<endl;
+        cout<<"1. Ingresar dinero en cuenta"<<endl;
+        cout<<"2. Retirar dinero en cuenta"<<endl;
+        cout<<"3. Consultar dinero de cuenta"<<endl;
+        cout<<"4. Salir"<<endl;
+        cin>>opc;
+        switch (opc){
+        case 1:
+            cout<<"Indique el monto que ingresara a su cuenta: "<<endl;
+            cin>>aux;
+            saldo+=aux;
+            break;
+        case 2:
+            cout<<"Indique el monto que retirara: "<<endl;
+            cin>>aux;
+            if (aux>saldo){
+                cout<<"El saldo que desea retirar es mayor al disponible en la cuenta"<<endl;
+            }else{
+                saldo-=aux;
+                cout<<"Su saldo es ["<<saldo<<"]"<<endl;
+            }
+            break;
+        case 3:
+            cout<<"Su saldo es ["<<saldo<<"]"<<endl;
+            break;
+        case 4:
+            cout<<"Hasta la proxima. Banco de Venezuela siempre a su servicio"<<endl;
+            break;
+        default:
+            cout<<"Opcion incorrecta"<<endl;
+            break;
+        }
+    } while (opc != 4);
+    return 0;
+}
