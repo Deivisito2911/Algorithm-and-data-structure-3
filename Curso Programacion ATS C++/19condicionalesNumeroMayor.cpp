9ea4c2a6074d481e88587b9c598d4510ee034df6
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-12 20:18:41 -0400

    Calcular numero mayor

---
+++ Curso Programacion ATS C++/19condicionalesNumeroMayor.cpp
@@ -0,0 +1,18 @@
+/*Escriba un programa que lea dos numeros y determine cual de ellos es 
+el mayor*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 0, b = 0;
+    cout<<"Introduzca dos numeros: "<<endl;
+    cin>>a>>b;
+    if (a>b){
+        cout<<"El numero mayor es: "<<a<<endl;
+    }else if (a<b){
+        cout<<"El numero mayor es: "<<b<<endl;
+        }else{
+            cout<<"Los numeros son iguales"<<endl;
+        }
+    return 0;
+}
