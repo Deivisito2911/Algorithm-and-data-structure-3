d9b86141cd58be43e9630fceb2e2a390bae92636
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-14 00:21:13 -0400

    Sentencia For

---
+++ Curso Programacion ATS C++/30sentenciaFor.cpp
@@ -0,0 +1,20 @@
+/*La sentencia for
+
+    for(exore1;expresion logica; expr2){
+        conjunto de instrucciones;
+    }
+*/
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+    
+    for (int i = 0; i <= 10; i++){
+        cout<<i<<endl;
+    }
+
+    system("pause");
+    return 0;
+}
