ef321237eb4e129ea22876acc5a34c3eebc92746
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-27 17:55:43 -0400

    Potencia manual

--- Curso Programacion ATS C++/36potenciaManual.cpp
+++ Curso Programacion ATS C++/36potenciaManual.cpp
@@ -1,1 +1,20 @@
-/*E*/
+/*Escriba un programa que calcule x^y, donde tanto x como y son enteros
+positivos, sin utilizar la funcion pow*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int x = 0, y = 0, resultado = 1;
+    cout<<"indique el numero base: "<<endl;
+    cin>>x;
+    cout<<"Indique a que numero lo elevara: "<<endl;
+    cin>>y;
+    for (int i = 0; i <= y-1; i++){
+        resultado*=x;
+    }
+    cout<<"El resultado de la potencia es: "<<resultado<<endl;
+    getche();
+    return 0;
+}
