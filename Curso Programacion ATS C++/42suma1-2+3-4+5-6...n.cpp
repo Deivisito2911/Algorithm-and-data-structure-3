403d1701ecb99ceceb726d2d4f98bbe23a2a43f5
Deivith Zanella <correoserio29112002@gmail.com>
2024-11-27 20:05:11 -0400

    sumar 2^1+2^2+2^3+...+2^n

---
+++ Curso Programacion ATS C++/42suma1-2+3-4+5-6...n.cpp
@@ -0,0 +1,19 @@
+/*Hacer un programa que calcule el resultado de la siguiente expresion:
+1-2+3-4+5-6...n*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int suma = 0, n = 0, resultado = 0;
+    cout<<"Indiqueme el valor de la suma: ";
+    cin>>n;
+    for (int i = 1; i <= n; i+=2){
+        resultado= i-(i+1);
+        suma+=resultado;
+    }
+    cout<<"El resultado es: "<<suma<<endl;
+    getche();
+    return 0;
+}
