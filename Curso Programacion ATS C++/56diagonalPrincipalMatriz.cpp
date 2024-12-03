e04758d3ad0d71b8bc66939a9ce54c7e9e7e5490
Deivith Zanella <correoserio29112002@gmail.com>
2024-12-03 11:48:39 -0400

    Diagonal principal de una matriz

---
+++ Curso Programacion ATS C++/56diagonalPrincipalMatriz.cpp
@@ -0,0 +1,28 @@
+/*Realiza un programa que deifna una amtriz de 3x3 y escriba un ciclo para que
+muestre la diagonal principal de la matriz*/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    const int fila = 3, columna = 3;
+    int matriz[fila][columna], contador = 0;
+    for (int i = 0; i < fila; i++){
+        for (int i2 = 0; i2 < columna; i2++){
+            matriz[i][i2]=contador;
+            contador++;
+        }
+    }
+    for (int i = 0; i < fila; i++){
+        for (int i2 = 0; i2 < columna; i2++){
+            if (i == i2){
+                cout<<"["<<matriz[i][i2]<<"]";
+            }else{
+                cout<<"[ ]";
+            }
+        }
+        cout<<endl;
+    }
+    getch();
+    return 0;
+}
